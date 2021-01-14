f5369d77a6694dc82de19f5a65790c91aff3a190
Lachlan Borwick <WoklanGaming@gmail.com>
2021-01-14 16:25:51 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connect4Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
